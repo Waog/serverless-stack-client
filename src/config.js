345a722a3f7df77c368fe3cc3a414a15186d55b6
waog <oliver.stadie@gmail.com>
2020-02-19 02:28:01 +0100

    ADD AWS configuration, Amplify
    - https://serverless-stack.com/chapters/configure-aws-amplify.html

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-central-1",
+    BUCKET: "waog-serverless-stack-tutorial"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://3wvyqbbntg.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_kbYKKeok7",
+    APP_CLIENT_ID: "t7tdcakv6cop5p4n11bsjcb9o",
+    IDENTITY_POOL_ID: "us-east-1:2d919867-9e36-40df-a7b8-69d301ec51e2"
+  }
+};
