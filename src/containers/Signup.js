ff9fab24d65e292b8bcbe11f1bd20d0eb4a31167
waog <oliver.stadie@gmail.com>
2020-02-20 22:53:00 +0100

    ADD signup & verify with AWS amplify
    - https://serverless-stack.com/chapters/signup-with-aws-cognito.html

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,3 +1,4 @@
+import { Auth } from "aws-amplify";
 import React, { useState } from "react";
 import {
   ControlLabel,
@@ -36,15 +37,34 @@ export default function Signup(props) {
 
     setIsLoading(true);
 
-    setNewUser("test");
-
-    setIsLoading(false);
+    try {
+      const newUser = await Auth.signUp({
+        username: fields.email,
+        password: fields.password
+      });
+      setIsLoading(false);
+      setNewUser(newUser);
+    } catch (e) {
+      alert(e.message);
+      setIsLoading(false);
+    }
   }
 
   async function handleConfirmationSubmit(event) {
     event.preventDefault();
 
     setIsLoading(true);
+
+    try {
+      await Auth.confirmSignUp(fields.email, fields.confirmationCode);
+      await Auth.signIn(fields.email, fields.password);
+
+      props.userHasAuthenticated(true);
+      props.history.push("/");
+    } catch (e) {
+      alert(e.message);
+      setIsLoading(false);
+    }
   }
 
   function renderConfirmationForm() {
