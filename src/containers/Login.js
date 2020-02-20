574091d342a530061a6cb7801759add003790b8f
waog <oliver.stadie@gmail.com>
2020-02-20 08:07:15 +0100

    ADD Login with AWS Cognito
    - https://serverless-stack.com/chapters/login-with-aws-cognito.html

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,3 +1,4 @@
+import { Auth } from "aws-amplify";
 import React, { useState } from "react";
 import { Button, ControlLabel, FormControl, FormGroup } from "react-bootstrap";
 import "./Login.css";
@@ -10,8 +11,15 @@ export default function Login(props) {
     return email.length > 0 && password.length > 0;
   }
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
+
+    try {
+      await Auth.signIn(email, password);
+      alert("Logged in");
+    } catch (e) {
+      alert(e.message);
+    }
   }
 
   return (
