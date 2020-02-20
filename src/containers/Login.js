5c786da28b84c6e2b66ea647a9c6b670bfbc82c8
waog <oliver.stadie@gmail.com>
2020-02-20 14:16:54 +0100

    ADD Login Loading State (spinner)
    - https://serverless-stack.com/chapters/give-feedback-while-logging-in.html

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,11 +1,13 @@
 import { Auth } from "aws-amplify";
 import React, { useState } from "react";
-import { Button, ControlLabel, FormControl, FormGroup } from "react-bootstrap";
+import { ControlLabel, FormControl, FormGroup } from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
 import "./Login.css";
 
 export default function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
@@ -14,12 +16,15 @@ export default function Login(props) {
   async function handleSubmit(event) {
     event.preventDefault();
 
+    setIsLoading(true);
+
     try {
       await Auth.signIn(email, password);
       props.userHasAuthenticated(true);
       props.history.push("/");
     } catch (e) {
       alert(e.message);
+      setIsLoading(false);
     }
   }
 
@@ -43,9 +48,15 @@ export default function Login(props) {
             type="password"
           />
         </FormGroup>
-        <Button block bsSize="large" disabled={!validateForm()} type="submit">
+        <LoaderButton
+          block
+          type="submit"
+          bsSize="large"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
           Login
-        </Button>
+        </LoaderButton>
       </form>
     </div>
   );
