7450ecd256c9b62a155d7365cd621cd4b49d2488
waog <oliver.stadie@gmail.com>
2020-02-21 20:03:37 +0100

    ADD redirect from unreachable pages
    - summarized 2 chapters into 1 commit:
    -- because the first chapter was useless by itself
    -- https://serverless-stack.com/chapters/create-a-route-that-redirects.html
    -- https://serverless-stack.com/chapters/use-the-redirect-routes.html

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import AppliedRoute from "./components/AppliedRoute";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import NewNote from "./containers/NewNote";
@@ -13,26 +15,31 @@ export default function Routes({ appProps }) {
   return (
     <Switch>
       <AppliedRoute path="/" exact component={Home} appProps={appProps} />
-      <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
-      <AppliedRoute
+      <UnauthenticatedRoute
+        path="/login"
+        exact
+        component={Login}
+        appProps={appProps}
+      />
+      <UnauthenticatedRoute
         path="/signup"
         exact
         component={Signup}
         appProps={appProps}
       />
-      <AppliedRoute
+      <AuthenticatedRoute
         path="/settings"
         exact
         component={Settings}
         appProps={appProps}
       />
-      <AppliedRoute
+      <AuthenticatedRoute
         path="/notes/new"
         exact
         component={NewNote}
         appProps={appProps}
       />
-      <AppliedRoute
+      <AuthenticatedRoute
         path="/notes/:id"
         exact
         component={Notes}
